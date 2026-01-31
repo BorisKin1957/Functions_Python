89c4c9938efb5d201a02b0b0e2165c9bdf939148
BorisKin1957 <6063354@gmail.com>
2026-01-31 14:41:51 +0700

    2.1 Функция map() — трансформация данных

--- temp.py
+++ temp.py
@@ -1,16 +1,1 @@
-def countdown(n):
-    """
-  Эта функция-генератор должна выдавать числа от n до 1.
-  Например, для n=3 она должна выдать 3, потом 2, потом 1.
-  """
-    # Напишите ваш код здесь
-    result = []
-    if n > 0:
-        for i in range(n):
-            result.append(n - i)
-    return result
-
-
-# Этот код не нужно отправлять в решение, он для проверки
-for number in countdown(5):
-    print(number)
+print(list(map(lambda wrd: wrd[0].upper(), input().split())))
